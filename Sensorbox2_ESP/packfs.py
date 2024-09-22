46ec7fe8162c565001249902368c53375ce955f5
dingo35 <spaarder@hotmail.nl>
2024-09-22 09:45:22 -0400

    packfs.py: gzip some data/ files

--- Sensorbox2_ESP/packfs.py
+++ Sensorbox2_ESP/packfs.py
@@ -13,8 +13,13 @@ try:
     # now gzip the stuff except zones.csv since this file is not served by mongoose but directly accessed:
     for file in os.listdir("data"):
         filename = os.fsdecode(file)
-        shutil.copy('data/' + filename, 'pack.tmp/data/' + filename)
-        filelist.append('data/' + filename)
+        if filename == "index.html" or filename == "styling.css" or filename == "update2.html":
+            with open('data/' + filename, 'rb') as f_in, gzip.open('pack.tmp/data/' + filename + '.gz', 'wb') as f_out:
+                f_out.writelines(f_in)
+            filelist.append('data/' + filename + '.gz')
+        else:
+            shutil.copy('data/' + filename, 'pack.tmp/data/' + filename)
+            filelist.append('data/' + filename)
     os.chdir('pack.tmp')
     cmdstring = 'python ../pack.py ' + ' '.join(filelist)
     os.system(cmdstring + '>../src/packed_fs.c')
